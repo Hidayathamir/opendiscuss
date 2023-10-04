c2c26f0ea28f1770f6d2c3a27d0d74c970442a20
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-05 02:00:30 +0700

    feat: add unit test service create question

---
+++ api/v1/question/service/create_question_test.go
@@ -0,0 +1,115 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/Hidayathamir/opendiscuss/api/v1/question/dto"
+	"github.com/Hidayathamir/opendiscuss/api/v1/question/repository"
+	"github.com/Hidayathamir/opendiscuss/mocks"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestQuestionService_CreateQuestion(t *testing.T) {
+	type fields struct {
+		repo repository.IQuestionRepository
+	}
+	type args struct {
+		ctx context.Context
+		req dto.ReqCreateQuestion
+	}
+
+	type testStruct struct {
+		name    string
+		fields  fields
+		args    args
+		want    int
+		wantErr bool
+	}
+	tests := []testStruct{}
+	{
+		Error_Validate := func() testStruct {
+			return testStruct{
+				name:    "if validate req error, should return error",
+				fields:  fields{},
+				args:    args{req: dto.ReqCreateQuestion{}},
+				want:    0,
+				wantErr: true,
+			}
+		}
+
+		Error_CreateQuestion_User_Not_Found := func() testStruct {
+			repo := mocks.NewIQuestionRepository(t)
+			req := dto.ReqCreateQuestion{UserID: 1, Question: "dummy question"}
+
+			repo.
+				On("CreateQuestion", mock.Anything, req.ToModelQuestion()).
+				Return(0, errors.New("dummy error questions_ibfk_1"))
+
+			return testStruct{
+				name:    "if create question error user not found, should return error",
+				fields:  fields{repo: repo},
+				args:    args{req: req},
+				want:    0,
+				wantErr: true,
+			}
+		}
+
+		Error_CreateQuestion := func() testStruct {
+			repo := mocks.NewIQuestionRepository(t)
+			req := dto.ReqCreateQuestion{UserID: 1, Question: "dummy question"}
+
+			repo.
+				On("CreateQuestion", mock.Anything, req.ToModelQuestion()).
+				Return(0, errors.New("dummy error"))
+
+			return testStruct{
+				name:    "if create question error, should return error",
+				fields:  fields{repo: repo},
+				args:    args{req: req},
+				want:    0,
+				wantErr: true,
+			}
+		}
+
+		Success := func() testStruct {
+			repo := mocks.NewIQuestionRepository(t)
+			req := dto.ReqCreateQuestion{UserID: 1, Question: "dummy question"}
+
+			want := 123
+			repo.
+				On("CreateQuestion", mock.Anything, req.ToModelQuestion()).
+				Return(want, nil)
+
+			return testStruct{
+				name:    "if create question error, should return error",
+				fields:  fields{repo: repo},
+				args:    args{req: req},
+				want:    want,
+				wantErr: false,
+			}
+		}
+
+		tests = append(tests, Error_Validate())
+		tests = append(tests, Error_CreateQuestion_User_Not_Found())
+		tests = append(tests, Error_CreateQuestion())
+		tests = append(tests, Success())
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			qs := &QuestionService{
+				repo: tt.fields.repo,
+			}
+			got, err := qs.CreateQuestion(tt.args.ctx, tt.args.req)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QuestionService.CreateQuestion() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("QuestionService.CreateQuestion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
