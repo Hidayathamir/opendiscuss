b94a4795b732d307c02bc9069a51f9f92687d9b1
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-04 18:05:06 +0700

    feat: add column created_at and updated_at

--- migrations/init-table.sql
+++ migrations/init-table.sql
@@ -1,79 +1,103 @@
 CREATE TABLE `vote_options` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) UNIQUE NOT NULL
+  `name` varchar(255) UNIQUE NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(255) UNIQUE NOT NULL
+  `username` varchar(255) UNIQUE NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `user_question_votes` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `question_id` int NOT NULL,
-  `vote_option_id` int NOT NULL
+  `vote_option_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `questions` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `body` varchar(255) NOT NULL
+  `body` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `question_statistics` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `question_id` int NOT NULL,
   `thumbs_up` int NOT NULL,
-  `thumbs_down` int NOT NULL
+  `thumbs_down` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `user_answer_votes` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `answer_id` int NOT NULL,
-  `vote_option_id` int NOT NULL
+  `vote_option_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `answers` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `questions_id` int NOT NULL,
   `user_id` int NOT NULL,
-  `body` varchar(255) NOT NULL
+  `body` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `answer_statistics` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `answer_id` int NOT NULL,
   `thumbs_up` int NOT NULL,
-  `thumbs_down` int NOT NULL
+  `thumbs_down` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `user_comment_votes` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `comment_id` int NOT NULL,
-  `vote_option_id` int NOT NULL
+  `vote_option_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `comments` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `answer_id` int NOT NULL,
   `user_id` int NOT NULL,
-  `body` varchar(255) NOT NULL
+  `body` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `comment_statistics` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `comment_id` int NOT NULL,
   `thumbs_up` int NOT NULL,
-  `thumbs_down` int NOT NULL
+  `thumbs_down` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE `parent_child_comments` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `parent_comment_id` int NOT NULL,
-  `child_comment_id` int NOT NULL
+  `child_comment_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT (now()),
+  `updated_at` datetime NOT NULL DEFAULT (now())
 );
 
 CREATE UNIQUE INDEX `user_question_votes_index_0` ON `user_question_votes` (`user_id`, `question_id`);
