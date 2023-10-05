b3f1b799e96010806f530ac3545274bd3ff53410
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-05 17:05:42 +0700

    feat: add service test

---
+++ api/v1/question/service/service_test.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Hidayathamir/opendiscuss/api/v1/question/repository"
+)
+
+func TestNewQuestionService(t *testing.T) {
+	type args struct {
+		repo repository.IQuestionRepository
+	}
+	type testStruct struct {
+		name string
+		args args
+		want IQuestionService
+	}
+
+	tests := []testStruct{
+		{
+			name: "NewQuestionService should return IQuestionService implementation",
+			args: args{},
+			want: &QuestionService{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewQuestionService(tt.args.repo); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewQuestionService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
