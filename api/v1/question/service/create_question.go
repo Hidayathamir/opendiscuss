6ec7081f16f5c716d6aaed709af564d77fefef94
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-05 16:57:01 +0700

    feat: add error context when create question but user not found

--- api/v1/question/service/create_question.go
+++ api/v1/question/service/create_question.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"strings"
 
 	"github.com/Hidayathamir/opendiscuss/api/v1/question/dto"
 	"github.com/Hidayathamir/opendiscuss/constant"
@@ -15,7 +16,11 @@ func (qs *QuestionService) CreateQuestion(ctx context.Context, req dto.ReqCreate
 
 	questionID, err := qs.repo.CreateQuestion(ctx, req.ToModelQuestion())
 	if err != nil {
-		return 0, errors.Wrap(err, "error create question")
+		err = errors.Wrap(err, "error create question")
+		if strings.Contains(err.Error(), "REFERENCES `users` (`id`)") {
+			return 0, errors.Wrap(err, "user not found")
+		}
+		return 0, err
 	}
 
 	return questionID, err
