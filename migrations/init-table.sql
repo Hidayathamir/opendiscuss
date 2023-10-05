e5a9bc4b70f746f7cdac50c13277ed5a8312f4d4
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-05 10:51:05 +0700

    feat: add column password table user

--- migrations/init-table.sql
+++ migrations/init-table.sql
@@ -8,6 +8,7 @@ CREATE TABLE `vote_options` (
 CREATE TABLE `users` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `username` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT (now()),
   `updated_at` datetime NOT NULL DEFAULT (now())
 );
--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ import "time"
 type User struct {
 	ID        int       `gorm:"column:id"`
 	Username  string    `gorm:"column:username"`
+	Password  string    `gorm:"column:password"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
