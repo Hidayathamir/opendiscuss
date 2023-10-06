6edbc78dfafc412c3647424f9bfdca9ce10c3f19
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-06 15:16:51 +0700

    fix: json key question highlight

--- api/v1/question/dto/get_question_list.go
+++ api/v1/question/dto/get_question_list.go
@@ -11,6 +11,6 @@ type QuestionHighlight struct {
 	Question   string    `json:"question"`
 	ThumbsUp   string    `json:"thumbs_up"`
 	ThumbsDown string    `json:"thumbs_down"`
-	CreatedAt  time.Time `gorm:"column:created_at"`
-	UpdatedAt  time.Time `gorm:"column:updated_at"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
