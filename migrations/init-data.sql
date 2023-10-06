edd840f1e8643ce56b10820699dcce29df101977
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-07 05:53:52 +0700

    feat: add data table vote_options

--- migrations/init-data.sql
+++ migrations/init-data.sql
@@ -1,1 +1,5 @@
 INSERT INTO users (username, password, created_at, updated_at) VALUES('hidayat', '$2a$10$Dm9fvdQkgaOjrB8aX/xzyumi6pnY7VTFU/83ExSZRGnlsTUL6gwm.', '2023-10-05 16:08:47', '2023-10-05 16:08:47');
+
+INSERT INTO vote_options (name, created_at, updated_at) VALUES('thumbs up', '2023-10-06 09:26:43', '2023-10-06 09:26:43');
+INSERT INTO vote_options (name, created_at, updated_at) VALUES('thumbs down', '2023-10-06 09:27:43', '2023-10-06 09:27:43');
+INSERT INTO vote_options (name, created_at, updated_at) VALUES('cancel', '2023-10-06 09:28:43', '2023-10-06 09:28:43');
