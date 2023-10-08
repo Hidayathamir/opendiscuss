dacf590863686d82d3f89f05f86f7122545dfad7
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-08 19:51:49 +0700

    fix: api integration test create question
    check question detail

--- integration_test/question/create_question_test.py
+++ integration_test/question/create_question_test.py
@@ -4,7 +4,7 @@ from utils import utils
 def test_success():
     username = utils.generate_string()
     password = "dummy password"
-    utils.register_user(username, password)
+    user_id = utils.register_user_get_id(username, password)
     jwt_token = utils.login_user_get_token(username, password)
     question = "what is promise?"
 
@@ -12,4 +12,15 @@ def test_success():
 
     assert response["data"] != None
     assert response["data"]["question_id"] != 0
+    assert response["error"] == None
+
+    question_id = response["data"]["question_id"]
+    response = utils.get_question_detail(question_id)
+
+    assert response["data"] != None
+    assert response["data"]["question"]["author"] == username
+    assert response["data"]["question"]["author_id"] == user_id
+    assert response["data"]["question"]["question"] == question
+    assert response["data"]["question"]["thumbs_up"] == 0
+    assert response["data"]["question"]["thumbs_down"] == 0
     assert response["error"] == None
