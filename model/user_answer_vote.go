d30dbef0cca8b89233e8c7c2cd7ff830c402f309
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-09 15:17:42 +0700

    fix: incorrect model

--- model/user_answer_vote.go
+++ model/user_answer_vote.go
@@ -1,12 +1,16 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/Hidayathamir/opendiscuss/constant"
+)
 
 type UserAnswerVote struct {
-	ID         int       `gorm:"column:id"`
-	QuestionID int       `gorm:"column:question_id"`
-	ThumbsUp   int       `gorm:"column:thumbs_up"`
-	ThumbsDown int       `gorm:"column:thumbs_down"`
-	CreatedAt  time.Time `gorm:"column:created_at"`
-	UpdatedAt  time.Time `gorm:"column:updated_at"`
+	ID           int                 `gorm:"column:id"`
+	UserID       int                 `gorm:"column:user_id"`
+	AnswerID     int                 `gorm:"column:answer_id"`
+	VoteOptionID constant.VoteOption `gorm:"column:vote_option_id"`
+	CreatedAt    time.Time           `gorm:"column:created_at"`
+	UpdatedAt    time.Time           `gorm:"column:updated_at"`
 }
