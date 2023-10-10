f6e0b67fb2297109878035aff0149aa3af3240a5
Hidayat Hamir <hidayat.03@erajaya.com>
2023-10-10 12:09:42 +0700

    fix: model type

--- model/user_comment_vote.go
+++ model/user_comment_vote.go
@@ -1,12 +1,16 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/Hidayathamir/opendiscuss/constant"
+)
 
 type UserCommentVote struct {
-	ID           int       `gorm:"column:id"`
-	UserID       int       `gorm:"column:user_id"`
-	CommentID    int       `gorm:"column:comment_id"`
-	VoteOptionID int       `gorm:"column:vote_option_id"`
-	CreatedAt    time.Time `gorm:"column:created_at"`
-	UpdatedAt    time.Time `gorm:"column:updated_at"`
+	ID           int                 `gorm:"column:id"`
+	UserID       int                 `gorm:"column:user_id"`
+	CommentID    int                 `gorm:"column:comment_id"`
+	VoteOptionID constant.VoteOption `gorm:"column:vote_option_id"`
+	CreatedAt    time.Time           `gorm:"column:created_at"`
+	UpdatedAt    time.Time           `gorm:"column:updated_at"`
 }
